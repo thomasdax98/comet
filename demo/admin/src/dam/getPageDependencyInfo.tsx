57b2d5cbabe8f764c5b5d7cd1e5fc16d7e20c364
Thomas Dax <thomas.dax@vivid-planet.com>
2023-03-15 15:32:17 +0100

    feat(getPageDependencyInfo): add url to Page dependency info

--- demo/admin/src/dam/getPageDependencyInfo.tsx
+++ demo/admin/src/dam/getPageDependencyInfo.tsx
@@ -1,6 +1,8 @@
 import { gql } from "@apollo/client";
 import { GetRenderInfo } from "@comet/cms-admin";
 import { GQLPageDependencyQuery, GQLPageDependencyQueryVariables } from "@src/graphql.generated";
+import { Page } from "@src/pages/Page";
+import { categoryToUrlParam } from "@src/utils/pageTreeNodeCategoryMapping";
 import * as React from "react";
 import { FormattedMessage } from "react-intl";
 
@@ -28,12 +30,20 @@ export const getPageDependencyInfo: GetRenderInfo = async (id: string, { apolloC
         },
     });
 
+    const dependencyRoute = Page.resolveDependencyRoute(data.page, { rootColumn: dependencyData.rootColumnName, jsonPath: dependencyData.jsonPath });
+
     if (data.page.pageTreeNode === null) {
         throw new Error(`Could not find a PageTreeNode for Page with id ${id}`);
     }
+
+    const url = `${contentScope.match.url}/pages/pagetree/${categoryToUrlParam(data.page.pageTreeNode.category)}/${
+        data.page.pageTreeNode.id
+    }/edit/${dependencyRoute}`;
+
     return {
         type: <FormattedMessage id="comet.dam.dependencies.Page" defaultMessage="Page" />,
         name: data.page.pageTreeNode.name,
         secondaryInfo: data.page.pageTreeNode.path,
+        url: url,
     };
 };
