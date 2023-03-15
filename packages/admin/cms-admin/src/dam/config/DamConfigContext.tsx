e8b9be730c94a61fb1f835dac88af2808a9dbd64
Thomas Dax <thomas.dax@vivid-planet.com>
2023-03-15 13:16:44 +0100

    feat(DamConfigContext): add dependencyRenderInfoProvider to DamConfigContext

--- packages/admin/cms-admin/src/dam/config/DamConfigContext.tsx
+++ packages/admin/cms-admin/src/dam/config/DamConfigContext.tsx
@@ -1,9 +1,34 @@
+import { ApolloClient } from "@apollo/client";
 import * as React from "react";
+
+import { UseContentScopeApi } from "../../contentScope/Provider";
+import { GQLDependency } from "../../graphql.generated";
+
+export interface DamDependencyRenderInfoOptions {
+    data: Pick<GQLDependency, "rootGraphqlObjectType" | "rootId" | "rootColumnName" | "jsonPath">;
+    apolloClient: ApolloClient<unknown>;
+    contentScope: UseContentScopeApi;
+}
+
+export interface DamDependencyRenderInfo {
+    type: React.ReactNode;
+    name: React.ReactNode;
+    secondaryInfo?: React.ReactNode;
+    url?: string;
+}
+
+export type GetRenderInfo = (id: string, options: DamDependencyRenderInfoOptions) => Promise<DamDependencyRenderInfo> | DamDependencyRenderInfo;
 
 export interface DamConfig {
     additionalMimeTypes?: string[];
     scopeParts?: string[];
     enableLicenseFeature?: boolean;
+    dependencyRenderInfoProvider?: {
+        [key: string]: {
+            getRenderInfo: GetRenderInfo;
+            renderCustomContent?: (renderInfo: DamDependencyRenderInfo) => React.ReactNode;
+        };
+    };
 }
 
 export const DamConfigContext = React.createContext<DamConfig | undefined>(undefined);
