b8d6747a54f96747a68ad5df0a612c77054c6313
maximilianpelka <maximilian.pelka@vivid-planet.com>
2023-07-19 11:29:00 +0200

    Optimize text segments rendering for matched text

--- packages/admin/cms-admin/src/common/MarkedMatches.tsx
+++ packages/admin/cms-admin/src/common/MarkedMatches.tsx
@@ -18,16 +18,20 @@ export const MarkedMatches: React.FunctionComponent<{ text: string; matches: Tex
     for (let i = 0; i < matches.length - 1; i++) {
         const match = matches[i];
         if (!match.where || match.where === (isPathMatch ? "name" : "path")) {
-            continue;
+            textSegments.push(<Mark focused={match.focused}>{text.substring(match.start, match.end + 1)}</Mark>);
+            textSegments.push(text.substring(match.end + 1, matches[i + 1].start));
         }
-        textSegments.push(<Mark focused={match.focused}>{text.substring(match.start, match.end + 1)}</Mark>);
-        textSegments.push(text.substring(match.end + 1, matches[i + 1].start));
+        textSegments.push(text.substring(match.start, matches[i + 1].start));
     }
 
     const lastMatch = matches[matches.length - 1];
 
-    textSegments.push(<Mark focused={lastMatch.focused}>{text.substring(lastMatch.start, lastMatch.end + 1)}</Mark>);
-    textSegments.push(text.substring(lastMatch.end + 1));
+    if (!lastMatch.where || lastMatch.where === (isPathMatch ? "name" : "path")) {
+        textSegments.push(<Mark focused={lastMatch.focused}>{text.substring(lastMatch.start, lastMatch.end + 1)}</Mark>);
+        textSegments.push(text.substring(lastMatch.end + 1));
+    } else {
+        textSegments.push(text.substring(lastMatch.start));
+    }
 
     return (
         <Text>
