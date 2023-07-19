be4d35d96da718a1e502418006d2b027b5b4f004
maximilianpelka <maximilian.pelka@vivid-planet.com>
2023-07-19 10:23:06 +0200

    Add 'where' property to TextMatch type

--- packages/admin/cms-admin/src/common/MarkedMatches.tsx
+++ packages/admin/cms-admin/src/common/MarkedMatches.tsx
@@ -2,9 +2,13 @@ import { orange, yellow } from "@mui/material/colors";
 import { styled } from "@mui/material/styles";
 import * as React from "react";
 
-export type TextMatch = { start: number; end: number; focused: boolean };
+export type TextMatch = { start: number; end: number; focused: boolean; where?: "name" | "path" };
 
-export const MarkedMatches: React.FunctionComponent<{ text: string; matches: TextMatch[] }> = ({ text, matches }) => {
+export const MarkedMatches: React.FunctionComponent<{ text: string; matches: TextMatch[]; isPathMatch?: boolean }> = ({
+    text,
+    matches,
+    isPathMatch = false,
+}) => {
     if (matches.length === 0) {
         return <>{text}</>;
     }
@@ -13,6 +17,9 @@ export const MarkedMatches: React.FunctionComponent<{ text: string; matches: Tex
 
     for (let i = 0; i < matches.length - 1; i++) {
         const match = matches[i];
+        if (!match.where || match.where === (isPathMatch ? "name" : "path")) {
+            continue;
+        }
         textSegments.push(<Mark focused={match.focused}>{text.substring(match.start, match.end + 1)}</Mark>);
         textSegments.push(text.substring(match.end + 1, matches[i + 1].start));
     }
