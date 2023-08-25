c3358a79c32338671e890901251033b97c63a79b
Thomas Dax <thomas.dax@vivid-planet.com>
2023-08-25 13:57:18 +0200

    api: add shop module

---
+++ demo/api/src/shop/shop.module.ts
@@ -0,0 +1,18 @@
+import { MikroOrmModule } from "@mikro-orm/nestjs";
+import { Module } from "@nestjs/common";
+import { Customer } from "@src/shop/entities/customer.entity";
+import { Order } from "@src/shop/entities/order.entity";
+import { Product } from "@src/shop/entities/product.entity";
+import { CustomerResolver } from "@src/shop/generated/customer.resolver";
+import { CustomersService } from "@src/shop/generated/customers.service";
+import { ProductResolver } from "@src/shop/generated/product.resolver";
+import { ProductsService } from "@src/shop/generated/products.service";
+import { OrderResolver } from "@src/shop/order.resolver";
+import { OrdersService } from "@src/shop/orders.service";
+
+@Module({
+    imports: [MikroOrmModule.forFeature([Order, Product, Customer])],
+    providers: [OrderResolver, OrdersService, ProductResolver, ProductsService, CustomerResolver, CustomersService],
+    exports: [],
+})
+export class ShopModule {}
