08d1e3086bcd9ae953c3646906291a913dc994a6
Thomas Dax <thomas.dax@vivid-planet.com>
2023-08-26 15:28:23 +0200

    (broken) admin: use admin crud generator to generate pages, data grids and forms

--- demo/admin/crud-generator-config.ts
+++ demo/admin/crud-generator-config.ts
@@ -4,4 +4,17 @@ export default [
         target: "src/news/generated",
         entityName: "News",
     },
+    {
+        target: "src/shop/products/generated",
+        entityName: "Product",
+        rootBlocks: { image: { name: "DamImageBlock", import: "@comet/cms-admin" } },
+    },
+    {
+        target: "src/shop/orders/generated",
+        entityName: "Order",
+    },
+    {
+        target: "src/shop/customers/generated",
+        entityName: "Customer",
+    },
 ] satisfies CrudGeneratorConfig[];
